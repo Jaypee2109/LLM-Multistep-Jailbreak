f5769b8907cea08fe6ff5ccaf5360a71167853ce
Julian Nico Pielmaier <94388978+Jaypee2109@users.noreply.github.com>
2024-07-02 00:39:29 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,18 @@
-organization = "xxxx"
-api_key = 'xxxx'
-user_prompt_path = 'prompts/prompt_developer.txt'
-assistant_prompt_path = 'prompts/prompt_assistant.txt'
+api_key = "sk-msj-access-account-V1uUkLyFMU1YzDJPrPlNT3BlbkFJFplNYWQ6NJNqYYpoBgJo"
+user_prompt_path = "prompts/prompt_developer.txt"
+assistant_prompt_path = "prompts/prompt_assistant.txt"
 
 
 ### paths for email or phones pairs
-university_path = 'data/enron_top100_email.json'
-university_pred_path = 'path_to_store/pred'
+university_path = "data/enron_top100_email.json"
+university_pred_path = "path_to_store/pred"
 
-llm_data_path = 'xxx'
-llm_pred_path = 'xxx'
+llm_data_path = "xxx"
+llm_pred_path = "xxx"
 ### used for result analysis (.py)
-result_path = 'results_enron_phone_MC_update/'
+result_path = "results_enron_phone_MC_update/"
 
 
 #### used for enron email content analysis
-content_path = 'xxx/email-pred-0.csv'
-content_save_path = 'xxx/email-pred-0_update.csv'
+content_path = "xxx/email-pred-0.csv"
+content_save_path = "xxx/email-pred-0_update.csv"
